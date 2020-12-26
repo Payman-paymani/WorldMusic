b91062184d3985794351ffeac9b12a73423e9fe5
Mukhtar Ahmad <mukhtar_payman@yahoo.com>
2020-12-26 15:56:02 -0500

    Initial commit

---
+++ src/main/java/com/maoptech/WorldMusic/WorldMusicApplication.java
@@ -0,0 +1,13 @@
+package com.maoptech.WorldMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maoptech/WorldMusic/WorldMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maoptech.WorldMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
